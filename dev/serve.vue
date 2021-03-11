ac4201733a321af74bb571366842dcb7ccaf80c9
Gerard Keane <gerard.keane@dfkeane.com>
2021-03-11 20:32:20 +0000

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import GkButton from '@/gkButton.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    GkButton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <gkButton />
+  </div>
+</template>
